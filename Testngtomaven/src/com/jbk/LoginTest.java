a2c1405a33fcdff2dbb271a91b933c1bc5f2a91f
Sharp <Sharp@Vinayaak>
2019-08-26 13:12:57 +0530

    First Commit

---
+++ Testngtomaven/src/com/jbk/LoginTest.java
@@ -0,0 +1,10 @@
+package com.jbk;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+  @Test
+  public void f() {
+	  System.out.println("Hello...");
+  }
+}
---
+++ Testngtomaven/src/com/jbk/LoginTest2.java
@@ -0,0 +1,9 @@
+package com.jbk;
+
+import org.testng.annotations.Test;
+
+public class LoginTest2 {
+  @Test
+  public void f() {
+  }
+}
